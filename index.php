f78ef08e9ff908d459c3fc4e80d560750ae57486
hubshuja <shujauom@gmail.com>
2018-12-10 03:43:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
+ <?php
 $primeNumberArray = array();
 $isPrime = false;
 $totlaSumOfPrime =0;
@@ -38,5 +26,3 @@ for ($i = 2; $i<1000000; $i++) {
 
 print_r($grandTotalSUm);
         ?>
-    </body>
-</html>
